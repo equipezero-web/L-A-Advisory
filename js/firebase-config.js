8e7b57eae6d6be81dd641a267c828a339ce42c2a
equipezero-web <lucaalves17.20@gmail.com>
2026-09-23 10:26:45 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,12 +9,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "COLE_AQUI_SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJETO.appspot.com",
-  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
-  appId: "SEU_APP_ID"
+  apiKey: "AIzaSyC1TLAnzW2ohEGKSvDCsBSCCTI2MwG_EJE",
+  authDomain: "royal-advisory.firebaseapp.com",
+  projectId: "royal-advisory",
+  storageBucket: "royal-advisory.firebasestorage.app",
+  messagingSenderId: "345964504076",
+  appId: "1:345964504076:web:db70a89cc00c52c5aeba29"
 };
 
 const app = initializeApp(firebaseConfig);
